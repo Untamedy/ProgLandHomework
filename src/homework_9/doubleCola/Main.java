c04da540fee9bfba0682256ebaf1595d3abe1387
Untamedy <YBolshakova@on.seo>
2019-07-17 17:10:17 +0300

    no message

--- src/homework_9/doubleCola/Main.java
+++ src/homework_9/doubleCola/Main.java
@@ -1,27 +1,17 @@
 package homework_9.doubleCola;
 
-
-
 /**
  *
  * @author YBolshakova
  */
 public class Main {
+
     public static void main(String[] args) {
-           ColaMachine colaMachine = new ColaMachine(5);
-           
-           System.out.println("Queue befor:"); 
-          for(ColaMachine.Human h: colaMachine.getQueueByCola()){
-              System.out.println(h.toString());
-              
-          }
-          System.out.println("Queue after :"); 
-          for(ColaMachine.Human h: colaMachine.giveCola()){
-              System.out.println(h.toString());
-              
-          }
+        ColaMachine colaMachine = new ColaMachine(5);
+
+        System.out.println("Queue befor:" + colaMachine.getQueueByCola());
+        System.out.println("Queue after :" + colaMachine.giveCola());
+
     }
- 
-    
 
 }
