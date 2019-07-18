418d86883bf21e56fbd0843164bd009660737c0c
Untamedy <YBolshakova@on.seo>
2019-07-18 20:43:25 +0300

    hw_10_translator

--- src/homework_10/translator/Main.java
+++ src/homework_10/translator/Main.java
@@ -10,12 +10,12 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         String from = "folderOne\\English.in.txt";
-        String to = "folderOne\\Russian.out.txt";
+        String to = "folderOne\\Ukranian.out.txt";
         
         DictionaryCreator creater = new DictionaryCreator("folderOne\\dictionary.txt");
         HashMap<String, String> map =  creater.reader();       
         
-        creater.addNewWord("was", "быть");
+        creater.addNewWord("was", "бути");
         
         Translator t = new Translator(map, from, to);
         t.translate();
