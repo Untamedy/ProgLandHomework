09ae2bcc5844fa75860fa6239023bd5cf19ae360
Untamedy <YBolshakova@on.seo>
2019-07-18 18:18:15 +0300

    hw_10_art_hash

---
+++ src/homework_10/art/AlphabetReader.java
@@ -0,0 +1,64 @@
+package homework_10.art;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ *
+ * @author YBolshakova
+ */
+public class AlphabetReader {
+
+    private String path;
+    private String alphabet;
+    private HashMap<String,String> alpHashMap;
+
+    public AlphabetReader() {
+
+    }
+
+    public AlphabetReader(String path, String alphabet) {
+        this.path = path;
+        this.alphabet = alphabet;
+    }
+    
+    public HashMap<String,String> mapOfLetters(String [] art, String [] letters){
+       alpHashMap=new HashMap<>();
+       for(int i =0; i<letters.length;i++){
+           alpHashMap.put(letters[i],art[i]);
+       }        
+        return alpHashMap;
+    }
+
+    public String [] readArtAlphabet() {
+        StringBuffer s = new StringBuffer();
+        String [] artLetters = new String[26];
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            String line = "";
+            int i = 0;
+            while ((line = reader.readLine()) != null) {
+                if (!line.isEmpty()) {
+                    s.append(line).append("!");
+                }
+                else{
+                    artLetters[i]=s.toString();
+                    i++;
+                    s = new StringBuffer();
+                }
+            } 
+            artLetters[i]=s.toString();
+        } catch (IOException ex) {
+        }
+        return artLetters;
+    }    
+
+    public String[] letters(String alphabet) {
+        String[] letters = alphabet.split(",");
+        return letters;
+    }    
+
+}
--- src/homework_10/art/Main.java
+++ src/homework_10/art/Main.java
@@ -1,9 +1,30 @@
 package homework_10.art;
 
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author YBolshakova
  */
 public class Main {
+
+    public static void main(String[] args) {
+        
+        String alphabet = "А,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+        String path = "C:\\Users\\YBolshakova\\Documents\\gs-maven\\Homework\\folderOne\\alphabet.txt";
+        AlphabetReader reader = new AlphabetReader(path,alphabet);
+        HashMap<String,String> hash = reader.mapOfLetters(reader.readArtAlphabet(),reader.letters(alphabet));
+       /* String [] letters = reader.readArtAlphabet();
+        for (String s : letters) {
+            String[] str = s.split("!");
+            for (String string : str) {
+                System.out.println(string);
+            }
+
+        }*/
+        System.out.println(hash);
+       
+    }
 
 }
