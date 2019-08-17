8c3c38709fbad1d2740aa843e7b6838d8f5b1d7e
Untamedy <YBolshakova@on.seo>
2019-08-17 12:39:28 +0300

    task_3

--- src/javaPro/homework_2/yahooFinance/Query.java
+++
@@ -1,8 +0,0 @@
-package javaPro.homework_2.yahooFinance;
-
-public class Query {
-    public int count;
-    public String created;
-    public String lang;
-    public Results results;
-}
--- src/javaPro/homework_2/yahooFinance/Results.java
+++
@@ -1,5 +0,0 @@
-package javaPro.homework_2.yahooFinance;
-
-public class Results {
-    public Rate[] rate;
-}
